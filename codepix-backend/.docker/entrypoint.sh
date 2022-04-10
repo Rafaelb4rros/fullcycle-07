1736998b4b31344ab18dc2e929d6656e57c7f173
Rafael Barros <rafaelnogueirabarr@gmail.com>
2022-04-10 18:00:43 -0300

    build: initializing cadepix-backend  with nest/cli and docker

--- codepix-backend/.docker/entrypoint.sh
+++ codepix-backend/.docker/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-
 npm install
-npm run typeorm migration:run
-npm run console fixtures
 npm run start:dev
--- codepix-backend/src/main.ts
+++ codepix-backend/src/main.ts
@@ -1,28 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { ModelNotFoundExceptionFilter } from './exception-filters/model-not-found.exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.setGlobalPrefix('api');
-  app.useGlobalFilters(new ModelNotFoundExceptionFilter());
-  app.connectMicroservice({
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        brokers: [process.env.KAFKA_BROKER],
-      },
-      consumer: {
-        groupId:
-          !process.env.KAFKA_CONSUMER_GROUP_ID ||
-          process.env.KAFKA_CONSUMER_GROUP_ID === ''
-            ? 'my-consumer-' + Math.random()
-            : process.env.KAFKA_CONSUMER_GROUP_ID,
-      },
-    },
-  });
-  app.startAllMicroservices();
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
